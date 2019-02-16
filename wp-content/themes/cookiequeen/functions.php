0f97c8a3eb1a046a092c460a1b16409a2f3ca480
rachelle <leahrachelle.w@gmail.com>
2019-02-16 14:09:24 -0500

    Beginning to add fancybox

--- wp-content/themes/cookiequeen/functions.php
+++ wp-content/themes/cookiequeen/functions.php
@@ -409,7 +409,12 @@ function replace_loop_product_title() {
 }
 add_action( 'init', 'replace_loop_product_title' );
 
+add_action( 'after_setup_theme', 'yourtheme_setup' );
 
-
+function yourtheme_setup() {
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
+}
 
 ?>
