01f19efdae913c8fa22484ec82f28c0515ccf312
rachelle <leahrachelle.w@gmail.com>
2019-02-17 22:59:39 -0500

    Add gallery template

---
+++ wp-content/themes/cookiequeen/templates/gallery-page.php
@@ -0,0 +1,56 @@
+<?php
+/*
+* Template Name: Gallery Page
+* Template Post Type: page
+*/
+ ?>
+<?php get_header(); ?>
+
+
+
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+		<!-- post thumbnail -->
+		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+			<a data-fancybox="gallery" href="<?php the_post_thumbnail_url( 'full' ); ?>" title="<?php the_title(); ?>">
+				<?php the_post_thumbnail(array(300,300)); // Declare pixel size you need inside the array ?>
+			</a>
+		<?php endif; ?>
+		<!-- /post thumbnail -->
+
+		<!-- post title -->
+		<h2>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+		</h2>
+		<!-- /post title -->
+
+		<!-- post details -->
+		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+		<!-- /post details -->
+
+		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+
+		<?php edit_post_link(); ?>
+
+	</article>
+	<!-- /article -->
+
+<?php endwhile; ?>
+
+<?php else: ?>
+
+	<!-- article -->
+	<article>
+		<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+	</article>
+	<!-- /article -->
+
+<?php endif; ?>
+
+			<?php get_template_part('pagination'); ?>
+
+
+
+<?php get_footer(); ?>
